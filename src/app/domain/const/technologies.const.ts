4623dbbd4b29076019cfdec54401ac7633893573
Robert Martinovski <robertmartinovski0@gmail.com>
2026-09-18 14:41:55 +0200

    update 2.0

--- src/app/domain/const/technologies.const.ts
+++ src/app/domain/const/technologies.const.ts
@@ -7,6 +7,4 @@ export const TECHNOLOGIES: TechBadge[] = [
   { name: 'Adobe Illustrator', icon: 'assets/images/icons/adobe_illustrator.svg' },
   { name: 'Adobe Photoshop', icon: 'assets/images/icons/adobe_photoshop.svg' },
   { name: 'Adobe InDesign', icon: 'assets/images/icons/adobe_indesign.svg' },
-  { name: 'Figma', icon: 'assets/images/icons/figma.svg' },
-  { name: 'Canva', icon: 'assets/images/icons/canva.svg' },
 ];
